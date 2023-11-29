69c3d2a6c3dae81437930f783ad52a108d134029
Carlos Gabriel <140734200+Inojoza28@users.noreply.github.com>
2023-11-29 11:42:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let message = document.getElementById("message");
 
 function guessNumber() {
     numTries++;
+    let numInput = document.getElementById("numInput").value;
 
     if (numTries > maxTries) {
         message.innerHTML = "Você atingiu o limite de tentativas! Tente novamente.";
@@ -12,20 +13,23 @@ function guessNumber() {
         return;
     }
 
-    let numInput = document.getElementById("numInput").value;
+    let remainingAttempts = maxTries - numTries;
+    document.getElementById("attempts").innerHTML = `Você tem ${remainingAttempts} tentativas restantes`;
 
     if (numInput < randomNumber) {
-        message.innerHTML = "O número é maior! Tente novamente.";
+        message.innerHTML = `O número é maior! Tente novamente.`;
     } else if (numInput > randomNumber) {
-        message.innerHTML = "O número é menor! Tente novamente.";
+        message.innerHTML = `O número é menor! Tente novamente.`;
     } else {
-        message.innerHTML = "Parabéns! Você acertou o número!" + "<br><br><button onclick='playAgain()'>Jogar Novamente</button>";
+        message.innerHTML = `Parabéns! Você acertou o número em ${numTries} tentativas!` +
+            "<br><br><button onclick='playAgain()'>Jogar Novamente</button>";
     }
 }
 
 function playAgain() {
     randomNumber = Math.floor(Math.random() * 100) + 1;
     numTries = 0;
+    document.getElementById("attempts").innerHTML = `Você tem ${maxTries} tentativas`;
     message.innerHTML = "";
     document.getElementById("restartButton").style.display = "none";
 }
@@ -40,6 +44,7 @@ function restartGame() {
     changeBackgroundColor();
     randomNumber = Math.floor(Math.random() * 100) + 1;
     numTries = 0;
+    document.getElementById("attempts").innerHTML = `Você tem ${maxTries} tentativas`;
     document.getElementById("restartButton").style.display = "none";
 }
 
@@ -47,5 +52,4 @@ document.addEventListener('DOMContentLoaded', function () {
     changeBackgroundColor();
 });
 
-
-// Feito por Carlos G. J. I. Santos
+//Feito por Carlos G. J. I. Santos
